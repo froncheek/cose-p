43b29f3a4490fde5b24c4fb15205b7f81305b8aa
Benson Fabonan <bfabonan@gmail.com>
2019-09-09 03:40:51 +0800

    Added category and category item class, with nav bar and banner modification

--- src/client/main.js
+++ src/client/main.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ItemContainer from './ItemContainer';
 import Item from './Item';
+import CategoryContainer from './CategoryContainer';
+import Category from './Category';
+
 
 class Main extends React.Component{
     render() {
         return(
             <main className="centered">
                 <article>
-                    <section className="banner centered"><h1>Banner</h1></section>
+                    <section className="banner">
+                        <div className="categories-title">CATEGORIES</div>
+                        <CategoryContainer>
+                            <Category>Cat 1</Category>
+                            <Category>Cat 1</Category>
+                            <Category>Cat 1</Category>
+                            <Category>Cat 1</Category>
+                        </CategoryContainer>
+                    </section>
                     <ItemContainer>
                         <Item />
                         <Item />

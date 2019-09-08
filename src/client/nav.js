51c20854a494cf51e6df3f6dd3e08d4f79193829
Benson Fabonan <bfabonan@gmail.com>
2019-09-08 19:30:38 +0800

    Adding images folder

--- src/client/nav.js
+++ src/client/nav.js
@@ -3,8 +3,13 @@ import React from 'react';
 class Nav extends React.Component{
     render() {
         return(
-            <nav>
-                
+            <nav className='nav-bar'>
+                <div className='nav-logo'></div>
+                <div className='nav-search-bar'>
+                   <div className='nav-seach-input'>What are you trying to find?</div>
+                   <div className='nav-seach-button'></div>
+                </div>
+                <div className='nav-icons'></div>
             </nav>
         )
     }
